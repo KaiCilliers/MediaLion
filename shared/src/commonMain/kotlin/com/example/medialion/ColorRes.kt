ac51bfcd43edb1348a4f6bd999b9e13ff628e7e5
nadine <kaijohan428@gmail.com>
2023-04-16 07:20:10 +0100

    add some color values

--- shared/src/commonMain/kotlin/com/example/medialion/ColorRes.kt
+++ shared/src/commonMain/kotlin/com/example/medialion/ColorRes.kt
@@ -1,6 +1,8 @@
 package com.example.medialion
 
 object ColorRes {
+    val primary by lazy { SharedRes.colors.primary }
+    val primaryVariant by lazy { SharedRes.colors.primaryVariant }
     val secondary by lazy { SharedRes.colors.secondary }
     val background by lazy { SharedRes.colors.background }
 }
