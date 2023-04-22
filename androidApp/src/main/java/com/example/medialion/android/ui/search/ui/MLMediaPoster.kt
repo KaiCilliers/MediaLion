d765e0ff046111ba1ea2679ee6fdf1de674ea24c
nadine <kaijohan428@gmail.com>
2023-04-22 09:28:43 +0100

    provide aspect ratio to poster UI component

--- androidApp/src/main/java/com/example/medialion/android/ui/search/ui/MLMediaPoster.kt
+++ androidApp/src/main/java/com/example/medialion/android/ui/search/ui/MLMediaPoster.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +18,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
@@ -28,11 +30,10 @@ import com.example.medialion.domain.models.SimpleMediaItem
 @Composable
 fun MLMediaPoster(
     mediaItem: SimpleMediaItem,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Box(
         modifier = modifier
-            .height(200.dp)
             .aspectRatio(0.75f, matchHeightConstraintsFirst = true)
     ) {
         SubcomposeAsyncImage(
@@ -84,7 +85,8 @@ private fun MLMediaPosterPreview() {
                     id = "1234",
                     title = "Harry Potter and the Philosopher's stone",
                     posterUrl = "https://m.media-amazon.com/images/I/71QMoH7mSLL._AC_SL1500_.jpgx"
-                )
+                ),
+                modifier = Modifier.height(200.dp),
             )
         }
     }
