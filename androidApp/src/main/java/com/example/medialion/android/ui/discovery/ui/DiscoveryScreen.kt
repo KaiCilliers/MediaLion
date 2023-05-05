85fdaebffc3bbd953d714d5388ecb0c7e20a6ac4
nadine <kaijohan428@gmail.com>
2023-05-05 18:28:49 +0100

    categories WIP

--- androidApp/src/main/java/com/example/medialion/android/ui/discovery/ui/DiscoveryScreen.kt
+++ androidApp/src/main/java/com/example/medialion/android/ui/discovery/ui/DiscoveryScreen.kt
@@ -1,14 +1,90 @@
 package com.example.medialion.android.ui.discovery.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.medialion.android.R
 import com.example.medialion.android.theme.MediaLionTheme
+import com.example.medialion.android.ui.components.ui.BottomBar
+import com.example.medialion.android.ui.search.ui.MLMediaFavoriteListItem
+import com.example.medialion.android.ui.search.ui.MLTitledMediaRow
+import com.example.medialion.domain.models.MovieUiModel
 
 @Composable
-fun DiscoveryScreen() {
-    Text(text = "Hello")
-}
+fun DiscoveryScreen(
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier
+            .background(MaterialTheme.colors.background)
+
+    ){
+        Row (
+            modifier = modifier
+                .background(MaterialTheme.colors.background)
+                .padding(top = 16.dp)
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.back_arrow_icon),
+                contentDescription = "",
+                modifier = modifier
+                    .size(30.dp)
+
+                )
+            Spacer(modifier = modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.logo_icon),
+                contentDescription = "",
+                modifier = modifier
+                    .size(60.dp)
+
+                )
+            Spacer(modifier = modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.about_icon),
+                contentDescription = "",
+                modifier = modifier
+                    .size(30.dp)
+
+                )
+        }
+        FilterCategories()
+
+
+
+    MLTitledMediaRow(rowTitle = "Horror", movies = listOf(
+        MovieUiModel(1, "HP", true),
+        MovieUiModel(1, "HP", true),
+        MovieUiModel(1, "HP", true),
+        MovieUiModel(1, "HP", true),
+        MovieUiModel(1, "HP", true),
+    ), onMediaItemClicked = {}
+    )
+
+        BottomBar()
+
+        }
+
+
+
+    }
+
 
 @Preview
 @Composable
