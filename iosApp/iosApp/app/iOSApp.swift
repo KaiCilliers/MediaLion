0a4545ab64c678707a7742edf9f656510e62e17f
nadine <kaijohan428@gmail.com>
2023-05-06 15:44:20 +0100

    set app name for ios and android

--- iosApp/iosApp/app/iOSApp.swift
+++ iosApp/iosApp/app/iOSApp.swift
@@ -6,7 +6,7 @@ struct iOSApp: App {
 	var body: some Scene {
 		WindowGroup {
             NavigationView {
-                ContentView()
+                SearchScreen()
             }
             .navigationTitle("ContentView")
 		}
