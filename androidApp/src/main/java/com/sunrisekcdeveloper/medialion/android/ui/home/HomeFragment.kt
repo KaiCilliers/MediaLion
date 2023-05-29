759ae0a5557ea02b366b1c17c538da4b770a8b13
nadine <kaijohan428@gmail.com>
2023-05-29 16:55:48 +0200

    Android - Add home key and fragment

---
+++ androidApp/src/main/java/com/sunrisekcdeveloper/medialion/android/ui/home/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.sunrisekcdeveloper.medialion.android.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import com.sunrisekcdeveloper.medialion.android.theme.MediaLionTheme
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MediaLionTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ androidApp/src/main/java/com/sunrisekcdeveloper/medialion/android/ui/home/HomeKey.kt
@@ -0,0 +1,20 @@
+package com.sunrisekcdeveloper.medialion.android.ui.home
+
+import androidx.fragment.app.Fragment
+import com.sunrisekcdeveloper.medialion.android.app.BaseKey
+import com.sunrisekcdeveloper.medialion.android.ui.search.DiscoveryViewModel
+import com.sunrisekcdeveloper.medialion.android.ui.search.SearchViewModel
+import com.zhuinden.simplestack.ServiceBinder
+import com.zhuinden.simplestackextensions.servicesktx.add
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data object HomeKey : BaseKey() {
+    override fun instantiateFragment(): Fragment = HomeFragment()
+    override fun bindServices(serviceBinder: ServiceBinder) {
+        with(serviceBinder) {
+            add(SearchViewModel(backstack))
+            add(DiscoveryViewModel(backstack))
+        }
+    }
+}
