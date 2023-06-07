6f2f417cbff3d7a69e2709bcd58a90ef9b8eb26c
nadine <kaijohan428@gmail.com>
2023-06-07 16:08:12 +0200

    add some validation logic to rename collection use case

--- shared/src/commonMain/kotlin/com/sunrisekcdeveloper/medialion/domain/search/usecases/RenameCollectionUseCase.kt
+++ shared/src/commonMain/kotlin/com/sunrisekcdeveloper/medialion/domain/search/usecases/RenameCollectionUseCase.kt
@@ -9,14 +9,17 @@ interface RenameCollectionUseCase {
         private val collectionRepo: CollectionRepository
     ) : RenameCollectionUseCase {
         override suspend fun invoke(oldCollectionName: Title, newCollectionName: Title) {
-            // trimming should occur in Repo layer - usecases are suppose to just coordinate tasks and delegates work
-            // remove business logic
-            // Title should contain the logic that it should be trimmed!
-            // see more here --> https://itnext.io/domain-driven-android-building-a-model-which-makes-sense-badb774c606d
-            /*
-            Model should not only contain data but also business constraints, rules and behaviours.
-             */
-            collectionRepo.renameCollection(oldCollectionName, Title(newCollectionName.value.trim()))
+            // todo Title should never be blank
+            if (newCollectionName.value.trim().isNotBlank()) {
+                // trimming should occur in Repo layer - usecases are suppose to just coordinate tasks and delegates work
+                // remove business logic
+                // Title should contain the logic that it should be trimmed!
+                // see more here --> https://itnext.io/domain-driven-android-building-a-model-which-makes-sense-badb774c606d
+                /*
+                Model should not only contain data but also business constraints, rules and behaviours.
+                 */
+                collectionRepo.renameCollection(oldCollectionName, Title(newCollectionName.value.trim()))
+            }
         }
     }
 }
