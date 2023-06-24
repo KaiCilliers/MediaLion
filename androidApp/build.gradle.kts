e1621962f826465875c7f293a3019f8f78bbad9c
nadine <kaijohan428@gmail.com>
2023-06-24 13:39:59 +0100

    Android :: add package name suffix for debug build

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -26,6 +26,9 @@ android {
         }
     }
     buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+        }
         getByName("release") {
             isMinifyEnabled = false
         }
