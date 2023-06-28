f002481664c8adff3a17037eb176a0f542b227a2
nadine <kaijohan428@gmail.com>
2023-06-28 09:03:03 +0100

    Remove pre-populated collection data

--- shared/src/commonMain/kotlin/com/sunrisekcdeveloper/medialion/domain/search/usecases/SetupInitialCollectionUseCase.kt
+++ shared/src/commonMain/kotlin/com/sunrisekcdeveloper/medialion/domain/search/usecases/SetupInitialCollectionUseCase.kt
@@ -28,48 +28,7 @@ interface SetupInitialCollectionUseCase {
             if (prePackagedCollections.size < PRE_PACKAGED_COLLECTIONS.size) {
                 PRE_PACKAGED_COLLECTIONS.forEach { collectionName ->
                     collectionRepo.deleteCollection(Title(collectionName))
-                    when {
-                        collectionName == "Favorites" -> {
-                            val media = mutableListOf<MediaItem>()
-                            coroutineScope {
-                                launch {
-                                    movieRepository.popularMovies()
-                                        .take(10)
-                                        .onEach { media.add(it) }
-                                        .collect()
-                                }
-                                launch {
-                                    tvRepository.popularTV()
-                                        .take(10)
-                                        .onEach { media.add(it) }
-                                        .collect()
-                                }
-                            }
-                            media.shuffled().forEach {
-                                collectionRepo.addMediaToCollection(collectionName, it)
-                            }
-                        }
-                        collectionName == "Popular Movies" -> {
-                            val media = mutableListOf<MediaItem>()
-                            movieRepository.popularMovies()
-                                .take(20)
-                                .onEach { media.add(it) }
-                                .collect()
-                            media.forEach {
-                                collectionRepo.addMediaToCollection(collectionName, it)
-                            }
-                        }
-                        collectionName == "Popular TV Shows" -> {
-                            val media = mutableListOf<MediaItem>()
-                            tvRepository.popularTV()
-                                .take(20)
-                                .onEach { media.add(it) }
-                                .collect()
-                            media.forEach {
-                                collectionRepo.addMediaToCollection(collectionName, it)
-                            }
-                        }
-                    }
+                    collectionRepo.insertCollection(collectionName)
                 }
             }
         }
@@ -77,6 +36,6 @@ interface SetupInitialCollectionUseCase {
 
     companion object {
         val PRE_PACKAGED_COLLECTIONS =
-            listOf("Favorites", "Popular Movies", "Popular TV Shows")
+            listOf("Favorites")
     }
 }
