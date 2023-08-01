34c55d8d7e885fcba5ed57341724d93d823f02fd
nadine <kaijohan428@gmail.com>
2023-08-01 08:10:51 +0100

    fix referencing deleted class

--- shared/src/iosMain/kotlin/com/sunrisekcdeveloper/medialion/di/PlatformModule.kt
+++ shared/src/iosMain/kotlin/com/sunrisekcdeveloper/medialion/di/PlatformModule.kt
@@ -1,16 +1,21 @@
 package com.sunrisekcdeveloper.medialion.di
 
+import com.sunrisekcdeveloper.medialion.components.shared.TMDBClientNew
 import com.sunrisekcdeveloper.medialion.utils.DefaultDispatcherProvider
 import com.sunrisekcdeveloper.medialion.utils.DispatcherProvider
 import com.sunrisekcdeveloper.medialion.network.HttpClientFactory
 import com.sunrisekcdeveloper.medialion.database.MediaLionDatabase
 import com.sunrisekcdeveloper.medialion.storage.DatabaseDriverFactory
 import com.sunrisekcdeveloper.medialion.storage.MediaLionDatabaseFactory
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 actual val platformModule = module {
-    single<TMDBClient> {
-        TMDBClient.Default(HttpClientFactory().create())
+    single<TMDBClientNew> {
+        TMDBClientNew(
+            httpClient = HttpClientFactory().create(),
+            mapper = get(named(MapperNames.SingleMediaItemNames.responseToSingleMediaApiDto))
+        )
     }
     single<MediaLionDatabase> {
         MediaLionDatabaseFactory(DatabaseDriverFactory()).create()
