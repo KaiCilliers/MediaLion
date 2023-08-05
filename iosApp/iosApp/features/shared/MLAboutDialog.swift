967644f504e01e7b45bc517652e9c44bcf6ad583
nadine <kaijohan428@gmail.com>
2023-08-05 11:44:40 +0100

    ios - move about dialog to new package

---
+++ iosApp/iosApp/features/shared/MLAboutDialog.swift
@@ -0,0 +1,149 @@
+//
+//  CustomAboutDialog.swift
+//  iosApp
+//
+//  Created by Nadine Cilliers on 30/04/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct MLAboutDialog: View {
+    
+    @State private var offset: CGFloat = 1000
+    let onCloseAction: () -> Void
+    
+    var body: some View {
+        
+            VStack{
+                Text(StringRes.about)
+                    .customFont(.subtitle1)
+                    .padding(.horizontal)
+                    .padding(.bottom, 4)
+                    .foregroundColor(.textBlack)
+                
+                Text(StringRes.aboutVersionText)
+                    .customFont(.subtitle2)
+                    .padding(.horizontal)
+                    .padding(.bottom, 4)
+                    .foregroundColor(.textBlack)
+                
+                Text(StringRes.aboutHeading)
+                    .customFont(.h4)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 5)
+                    .foregroundColor(.textBlack)
+                
+                Text(StringRes.appDescription)
+                    .customFont(.body2)
+                    .lineSpacing(1)
+                    .padding(.bottom, 1)
+                    .foregroundColor(.textBlack)
+                
+                Text(StringRes.appDevelopers)
+                    .customFont(.body2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.textBlack)
+                
+                
+                Text(StringRes.appDevelopersNames)
+                    .customFont(.body2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 1 )
+                    .foregroundColor(.textBlack)
+                
+                Text(StringRes.graphicDesigner)
+                    .customFont(.body2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.textBlack)
+                
+                Text(StringRes.graphicDesignerName)
+                    .customFont(.body2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.textBlack)
+            }
+           
+            .padding()
+            .padding(.bottom)
+            .background(Color.dialogOrange)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .overlay {
+                VStack{
+                    HStack{
+                        
+                        Spacer()
+                        
+                        Button{
+                            close()
+                        }label: {
+                            Image("closeIcon")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                }
+                
+                .padding(.top, 25)
+                .padding(.trailing, 20)
+               
+            }
+            .shadow(radius: 10)
+            .padding(30)
+            .offset(x: 0, y: offset)
+            .onAppear{
+                withAnimation(.spring()) {
+                    offset = 0
+                }
+            }
+        }
+    
+        
+        func close() {
+            withAnimation(.spring()) {
+                offset = 1000
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                onCloseAction()
+            }
+        }
+    }
+
+
+struct CustomAboutDialog_Previews: PreviewProvider {
+    static var previews: some View {
+        MLAboutDialog(onCloseAction: {})
+    }
+}
+
+
+private struct AboutScreen: View {
+    
+    @State var isActive: Bool = false
+
+    var body: some View {
+        ZStack{
+            VStack{
+                Button{
+                    isActive = true
+                } label: {
+                    Image("aboutIcon").background(Color.background)
+                }
+            }
+            .padding()
+            
+            if isActive {
+                MLAboutDialog(onCloseAction: {})
+            }
+        }
+    }
+}
+
+struct AboutScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutScreen()
+    }
+}
